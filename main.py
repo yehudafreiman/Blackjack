0cfa5da65a9b4c8825483056813156ca91dba470
Yehuda Freiman <yehfre@icloud.com>
2025-10-30 12:23:36 +0200

    add func

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from core import game_logic
 
 if __name__=="__main__":
 
-    print(deck.build_standard_deck())
-    print(deck.shuffle_by_suit(deck=deck.build_standard_deck(), swaps=5000))
+    deck.build_standard_deck()
+    deck.shuffle_by_suit(deck=deck.build_standard_deck(), swaps=5000)
 
     player = {"hand": []}
     dealer = {"hand": []}
 
-    print(game_logic.run_full_game(deck.build_standard_deck(), player, dealer))
+    print(game_logic.run_full_game(deck.shuffle_by_suit(deck=deck.build_standard_deck(), swaps=5000), player, dealer))
 
 
